0e2c413b2ee991a47f3479e068cda41775948cd8
matil019 <yohashi1991@gmail.com>
2021-02-08 21:28:43 +0900

    Add --num

--- app/CmdArgs.hs
+++ app/CmdArgs.hs
@@ -6,10 +6,12 @@ import Options.Applicative
 data CommonArgs = CommonArgs
   { seedIn  :: Maybe FilePath
   , seedOut :: Maybe FilePath
+  , numRand :: Int
   }
   deriving (Eq, Show)
 
 commonArgsParser :: Parser CommonArgs
-commonArgsParser = (\seedIn seedOut -> CommonArgs{..})
+commonArgsParser = (\seedIn seedOut numRand -> CommonArgs{..})
   <$> optional (strOption (long "seed-in" <> metavar "FILE" <> help "RNG seed file (for reproducible random number)"))
   <*> optional (strOption (long "seed-out" <> metavar "FILE" <> help "File to output seed after generating a number. Can be the same file as --seed-in, in which case it is overwritten with a new seed"))
+  <*> option auto (long "num" <> metavar "INT" <> value 1 <> showDefault <> help "Number of random numbers to generate")
