97bd1c246d53e8f0993466475eea211698fcfed7
matil019 <yohashi1991@gmail.com>
2021-02-08 21:26:53 +0900

    Rename SeedArgs to CommonArgs

--- app/CmdArgs.hs
+++ app/CmdArgs.hs
@@ -3,13 +3,13 @@ module CmdArgs where
 
 import Options.Applicative
 
-data SeedArgs = SeedArgs
+data CommonArgs = CommonArgs
   { seedIn  :: Maybe FilePath
   , seedOut :: Maybe FilePath
   }
   deriving (Eq, Show)
 
-seedArgsParser :: Parser SeedArgs
-seedArgsParser = (\seedIn seedOut -> SeedArgs{..})
+commonArgsParser :: Parser CommonArgs
+commonArgsParser = (\seedIn seedOut -> CommonArgs{..})
   <$> optional (strOption (long "seed-in" <> metavar "FILE" <> help "RNG seed file (for reproducible random number)"))
   <*> optional (strOption (long "seed-out" <> metavar "FILE" <> help "File to output seed after generating a number. Can be the same file as --seed-in, in which case it is overwritten with a new seed"))
