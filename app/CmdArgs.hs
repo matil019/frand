90c373cd527bbf5016ba2f2a4fef053011a585e9
matil019 <yohashi1991@gmail.com>
2021-02-08 21:09:45 +0900

    Improve commandline help text

--- app/CmdArgs.hs
+++ app/CmdArgs.hs
@@ -11,5 +11,5 @@ data SeedArgs = SeedArgs
 
 seedArgsParser :: Parser SeedArgs
 seedArgsParser = (\seedIn seedOut -> SeedArgs{..})
-  <$> optional (strOption (long "seed-in" <> metavar "FILE" <> help "RNG seed file"))
-  <*> optional (strOption (long "seed-out" <> metavar "FILE" <> help "File to output seed after generating a number"))
+  <$> optional (strOption (long "seed-in" <> metavar "FILE" <> help "RNG seed file (for reproducible random number)"))
+  <*> optional (strOption (long "seed-out" <> metavar "FILE" <> help "File to output seed after generating a number. Can be the same file as --seed-in, in which case it is overwritten with a new seed"))
